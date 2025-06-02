04feadb7e1e75527240685ee7a2a7682511ac2e2
Djibril Nefzi <69733067+DjibriilN@users.noreply.github.com>
2025-06-02 09:54:43 +0200

    Add Gameicon to Discord RPC

--- src/Cemu/DiscordPresence/DiscordPresence.cpp
+++ src/Cemu/DiscordPresence/DiscordPresence.cpp
@@ -4,6 +4,46 @@
 
 #include <discord_rpc.h>
 #include "Common/version.h"
+#include "Cafe/TitleList/TitleList.h"
+#include <wx/filename.h>
+#include <wx/image.h>
+#include <wx/wfstream.h>
+#include <wx/imagpng.h>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+static std::string ExportCurrentGameIconAsPng(uint64_t titleId) {
+    TitleInfo titleInfo;
+    if (!CafeTitleList::GetFirstByTitleId(titleId, titleInfo))
+        return {};
+    std::string tempMountPath = TitleInfo::GetUniqueTempMountingPath();
+    if (!titleInfo.Mount(tempMountPath, "", FSC_PRIORITY_BASE))
+        return {};
+    auto tgaData = fsc_extractFile((tempMountPath + "/meta/iconTex.tga").c_str());
+    if (!tgaData) {
+        tgaData = fsc_extractFile((tempMountPath + "/meta/iconTex.tga.gz").c_str());
+        if (tgaData) {
+            auto decompressed = zlibDecompress(*tgaData, 70 * 1024);
+            std::swap(tgaData, decompressed);
+        }
+    }
+    std::string outPath;
+    if (tgaData && tgaData->size() > 16) {
+        wxMemoryInputStream tmp_stream(tgaData->data(), tgaData->size());
+        wxImage image(tmp_stream);
+        if (image.IsOk()) {
+            fs::path iconDir = ActiveSettings::GetUserDataPath("discord_icons");
+            if (!fs::exists(iconDir)) fs::create_directories(iconDir);
+            outPath = (iconDir / fmt::format("{:016x}.png", titleId)).string();
+            wxFileOutputStream pngFileStream(outPath);
+            wxPNGHandler pngHandler;
+            pngHandler.SaveFile(&image, pngFileStream, false);
+        }
+    }
+    titleInfo.Unmount(tempMountPath);
+    return outPath;
+}
 
 DiscordPresence::DiscordPresence()
 {
@@ -20,29 +60,46 @@ DiscordPresence::~DiscordPresence()
 
 void DiscordPresence::UpdatePresence(State state, const std::string& text) const
 {
-	DiscordRichPresence discord_presence{};
+    DiscordRichPresence discord_presence{};
 
-	std::string state_string, details_string;
-	switch (state)
-	{
-	case Idling:
-		details_string = "Idling";
-		break;
-	case Playing:
-		details_string = "Ingame";
-		state_string = "Playing " + text;
-		break;
-	default:
-		assert(false);
-		break;
-	}
+    std::string state_string, details_string;
+    std::string largeImageKey = "logo_icon_big_png";
+    std::string largeImageText = BUILD_VERSION_WITH_NAME_STRING;
 
-	discord_presence.details = details_string.c_str();
-	discord_presence.state = state_string.c_str();
-	discord_presence.startTimestamp = time(nullptr);
-	discord_presence.largeImageText = BUILD_VERSION_WITH_NAME_STRING;
-	discord_presence.largeImageKey = "logo_icon_big_png";
-	Discord_UpdatePresence(&discord_presence);
+    switch (state)
+    {
+    case Idling:
+        details_string = "Idling";
+        break;
+    case Playing:
+        details_string = "Ingame";
+        state_string = "Playing " + text;
+        // Versuche das Game-Icon zu exportieren
+        {
+            uint64_t titleId = CafeSystem::GetForegroundTitleId();
+            std::string iconPath = ExportCurrentGameIconAsPng(titleId);
+            if (!iconPath.empty()) {
+                // Discord RPC: largeImageKey muss ein Key sein, der bei Discord hochgeladen wurde.
+                // Workaround: Wir nutzen den Dateinamen als Key, falls das Asset im Discord-Dev-Portal hinterlegt ist.
+                // Alternativ: Immer logo_icon_big_png, aber largeImageText auf Spielnamen setzen.
+                largeImageKey = fmt::format("gameicon_{:016x}", titleId);
+                TitleInfo titleInfo;
+                if (CafeTitleList::GetFirstByTitleId(titleId, titleInfo))
+                    largeImageText = titleInfo.GetMetaTitleName();
+            }
+        }
+        break;
+    default:
+        assert(false);
+        break;
+    }
+
+    discord_presence.details = details_string.c_str();
+    discord_presence.state = state_string.c_str();
+    discord_presence.startTimestamp = time(nullptr);
+    discord_presence.largeImageText = largeImageText.c_str();
+    discord_presence.largeImageKey = largeImageKey.c_str();
+    Discord_UpdatePresence(&discord_presence);
 }
 
 void DiscordPresence::ClearPresence() const
